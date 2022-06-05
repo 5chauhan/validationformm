135d77cbe55da469d0c9767949a84db1539c4971
Sourabh Chauhan <sourabh.kumar35@gmail.com>
2022-06-05 11:29:45 +0530

    Updates

---
+++ precache-manifest.6005d48fd826767dd74de9847e4ca635.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "441b77198cfd30c830ff",
+    "url": "/static/css/main.dcbbc323.chunk.css"
+  },
+  {
+    "revision": "441b77198cfd30c830ff",
+    "url": "/static/js/main.441b7719.chunk.js"
+  },
+  {
+    "revision": "f8ed6f69171f2b2eef29",
+    "url": "/static/js/1.f8ed6f69.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "41a736aeec9c4d653a4a6ba3be256cb8",
+    "url": "/index.html"
+  }
+];
